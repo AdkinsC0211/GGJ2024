d2a966a2ed48b039ad01131abdf95255cd3a9422
AdkinsC0211 <adkinsc0211@gmail.com>
2024-01-27 11:45:57 -0500

    Made the gun fire and have recoil

---
+++ Scenes/Interactables/Arm.gd
@@ -0,0 +1,33 @@
+extends RigidBody3D
+
+const BULLET = preload("res://Scenes/Interactables/Bullet.tscn")
+var can_fire = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		if (event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT and can_fire):
+			apply_impulse(Vector3.UP*30, $Marker3D.transform.origin)
+			var temp = BULLET.instantiate()
+			get_tree().get_root().get_node("Main").get_node("WorldRoot").add_child(temp)
+			temp.global_transform = $Gun.global_transform
+			for i in range(15):
+				temp = BULLET.instantiate()
+				get_tree().get_root().get_node("Main").get_node("WorldRoot").add_child(temp)
+				temp.global_transform = $Gun.global_transform
+				temp.global_rotation_degrees.x += randf_range(-20, 20)
+				temp.global_rotation_degrees.z += randf_range(-20, 20)
+			$FireTimer.start()
+			can_fire = false
+
+
+func _on_timer_timeout():
+	can_fire = true
---
+++ Scenes/Interactables/Bullet.gd
@@ -0,0 +1,19 @@
+extends Area3D
+
+var lifespan = 5
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	translate_object_local(Vector3.UP*delta*25)
+	lifespan -= delta
+	if lifespan<0:
+		queue_free()
+
+
+func _on_body_entered(body):
+	if body.is_in_group("enemies"):
+		body.queue_free()
